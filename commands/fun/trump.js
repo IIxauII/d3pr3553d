194e3aca2a1706fcafb9c69a776e664463ba9106
Felix Hartick <failxtreme@icloud.com>
2019-03-08 18:30:03 +0100

    finished up trump command

--- commands/fun/trump.js
+++ commands/fun/trump.js
@@ -1,5 +1,10 @@
+const miniEmbed = require("../../lib/miniEmbed.js");
+const axios = require('axios');
+
 exports.run = (client, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    let baseUrl = "https://api.whatdoestrumpthink.com/api/";
+    let requestString = "";
     if (member) {
         personalizedMessage();
     } else {
@@ -7,11 +12,24 @@ exports.run = (client, message, args) => {
     }
 
     async function personalizedMessage() {
-        message.channel.send("mentioned:");
-        console.log(member.user.tag);
+        requestString = baseUrl + "v1/quotes/personalized?q=" + member.user.tag;
+        getTrumpRequest(requestString);
     }
 
     async function nonPersonalizedMessage() {
-        message.channel.send("nnon.personalized");
+        requestString = baseUrl + "v1/quotes/random";
+        getTrumpRequest(requestString);
+    }
+
+    async function getTrumpRequest(requestString) {
+        axios.get(requestString)
+        .then(response => {            
+            let trumpArray = [{"name": "Trump says:", "value": response.data.message}];
+
+            message.channel.send(miniEmbed.createMiniEmbed(client, "0xff9900", trumpArray));
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 }
