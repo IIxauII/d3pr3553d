285c10c5c5eb1bf9073d1667345b94e4d5065601
IIxauII <33882825+IIxauII@users.noreply.github.com>
2019-04-05 22:33:33 +0200

    added some typeracer garbage

--- events/message.js
+++ events/message.js
@@ -7,6 +7,40 @@ let admin1 = adminConfig.admins.user1; //user1
 let admin2 = adminConfig.admins.user2; //user2
 
 exports.run = (client,  message) => {
+    //console.log(message.author.id);
+    if (message.author.id == 474670778248331276) {
+        let attachments = message.attachments.array();
+        if (attachments && attachments.length > 0) {
+            attachments.forEach(function(attachment) {
+                console.log(attachment.url);
+                if (attachment.url) {                    
+                    let imgUrl = attachment.url;
+                    const tesseract = require('tesseract.js');
+                    tesseract.recognize(imgUrl, {
+                        lang: 'eng'
+                    })
+                    .then(function(result) {                        
+                        console.log(result.confidence);
+                        //let testChannel = client.channels.get('563804543645777921');
+                        //testChannel.send(imgUrl);
+                        //testChannel.send(result.text);
+                        //testChannel.send(result.confidence);
+                        let formattedText = result.text.replace(/\n/g," ");
+                        formattedText = formattedText.slice(0, -2);
+                        let textArray = formattedText.split(" ");
+                        let pauseTime = (textArray.length * 1000) / 2.5;
+                        console.log(formattedText);
+                        console.log(pauseTime + "ms");
+                        setTimeout(function() {
+                            message.channel.send(formattedText);
+                        }, pauseTime);
+                    })
+                    
+                }
+            })
+        }
+        return;
+    }    
     if (message.author.bot) return;
     if (message.content.indexOf(prefix) !== 0) return;
     if (message.author.id != admin1 && message.author.id != admin2) return;
