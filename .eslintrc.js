6bdb344272ccc6014bc5139b5173e5a2bd4ddce9
Felix Hartick <failxtreme@icloud.com>
2019-04-10 23:49:34 +0200

    added more eslint rules, fixed eslint styling issues in library folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: 'airbnb-base',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-      "linebreak-style": 'off',
-      "import/newline-after-import": 'off',
-      "import/no-dynamic-require": 'off',
-      "global-require": 'off',
-      "indent": ["error", 4],
-      "no-console": 'off' //for development purposes
-  },
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: 'airbnb-base',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        "linebreak-style": 'off',
+        "import/newline-after-import": 'off',
+        "import/no-dynamic-require": 'off',
+        "global-require": 'off',
+        "indent": ["error", 4],
+        "no-console": 'off', //for development purposes
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }]
+    },
 };
--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,17 @@ const fs = require('fs');
 
 exports.run = (client) => {
     let fileArray = [];
-    let object = {};
-    let options = {
-        encoding: "utf-8",
-        withFileTypes: true
-    }
+    const object = {};
+    const options = {
+        encoding: 'utf-8',
+        withFileTypes: true,
+    };
 
-    let folders = fs.readdirSync("./commands/", options);
-    folders.forEach(folder => {        
+    const folders = fs.readdirSync('./commands/', options);
+    folders.forEach((folder) => {
         if (folder.isDirectory()) {
-            let files = fs.readdirSync(`./commands/${folder.name}/`, options);
-            files.forEach(file => {
+            const files = fs.readdirSync(`./commands/${folder.name}/`, options);
+            files.forEach((file) => {
                 fileArray.push(file.name);
             });
         }
@@ -24,8 +24,8 @@ exports.run = (client) => {
     console.log(object);
     console.log("---"); */
 
-    let objectString = JSON.stringify(object, null, 4);
-    fs.writeFileSync("./configs/commands.json", objectString);
+    const objectString = JSON.stringify(object, null, 4);
+    fs.writeFileSync('./configs/commands.json', objectString);
 
     console.log(`Ready! Currently in ${client.channels.size} channels on ${client.guilds.size} servers\n${client.users.size} users`);
-}
+};
