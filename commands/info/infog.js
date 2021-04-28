8bc9524064552d750e3949fdd860355711bca36b
IIxauII <failxtreme@icloud.com>
2021-04-28 15:54:43 +0200

    adjusted guild info command & made uptime command pretty

--- commands/info/infog.js
+++ commands/info/infog.js
@@ -1,10 +1,38 @@
+const miniEmbed = require('../../lib/miniEmbed');
+const color = require('../../configs/colors.json');
+
 // eslint-disable-next-line no-unused-vars
 exports.run = (client, message, args) => {
-    message.delete();
+    //message.delete();
     const g = message.guild;
-    message.channel.send(`\`\`\`Markdown\n#Server Info:\n\`\`\`\n\`\`\`\nName: ${g.name}\nID: ${g.id
-    }\nMember Count: ${g.memberCount}\nOwner: ${g.owner.user.username}\nOwner ID: ${g.ownerID
-    }\nRegion: ${g.region}\nCreated At: ${g.createdAt}\nIcon URL: ${g.iconURL}\n\`\`\``);
-    // of you get the error that the propert "user" is undefinded,
-    // change g.owner.user.username to g.owner
+    const guildDataArray = [];
+
+    function gatherGuildData() {
+        createField('Name', g.name);
+        if (g.description) {
+            createField('Description', g.description);
+        }
+        createField('ID', g.id);
+        createField('Member Count', g.memberCount);
+        if (g.owner) {
+            createField('Owner', `${g.owner.user.username} ${g.ownerID}`);
+        } else {
+            createField('Owner', g.ownerID);
+        }
+        createField('Region', g.region);
+        createField('Created At', g.createdAt);
+        //createField('MFA Level', g.mfaLevel);
+        createField('Icon', g.iconURL());
+        createField('Default Notifications', g.defaultMessageNotifications.toLowerCase());
+        createField('Emoji Count', g.emojis.cache.array().length);
+        createField('Roles Count', g.roles.cache.array().length);
+        createField('Highest Role', g.roles.highest.name);
+    }
+
+    function createField(title, value) {
+        guildDataArray.push(miniEmbed.createMiniEmbedFields(title, value));
+    }
+
+    gatherGuildData();
+    message.channel.send(miniEmbed.createMiniEmbed(client, color.violet, guildDataArray));
 };
--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
+const microEmbed = require('../../lib/microEmbed');
+
 const {
     ms, s, m, h, d,
 } = require('time-convert');
 
 exports.run = (client, message, args) => {
     const upTime = ms.to(d, h, m, s)(client.uptime);
-    message.reply(`Client has been up for: ${upTime[0]} days, ${upTime[1]} hours, ${upTime[2]} minutes, ${upTime[3]} seconds!`);
+    message.channel.send(microEmbed.createMicroEmbed(client, `Client has been up for: ${upTime[0]} days, ${upTime[1]} hours, ${upTime[2]} minutes, ${upTime[3]} seconds`));
 };
